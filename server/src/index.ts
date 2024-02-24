b0096746479b84c2db218c7d7e38df6472bf37ae
Niladri Ghosh <niladri1406@gmail.com>
2024-02-24 20:27:15 +0530

    feat: add route for signup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,31 @@
 import { Hono } from "hono";
+import { sign } from "hono/jwt";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.post("/api/v1/signup", (c) => {
-  return c.text("signup route");
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
 });
 
 app.post("/api/v1/signin", (c) => {
