4a6e97fd6960ebc1a0f962d77c962f151e967d7c
Niladri Ghosh <niladri1406@gmail.com>
2024-02-25 17:59:07 +0530

    feat: add middleware for blogRoute -- authorization

--- server/src/routes/blog.ts
+++ server/src/routes/blog.ts
@@ -13,6 +13,22 @@ export const blogRouter = new Hono<{
   };
 }>();
 
+blogRouter.use(async (c, next) => {
+  const jwt = c.req.header("Authorization");
+  if (!jwt) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  const token = jwt.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  c.set("userId", payload.id);
+  await next();
+});
+
 blogRouter.post("/", async (c) => {
   const userId = c.get("userId");
   const prisma = new PrismaClient({
