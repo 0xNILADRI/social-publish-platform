18c38474416520508dce151cf5fe3785d990732a
Niladri Ghosh <niladri1406@gmail.com>
2024-02-25 17:50:42 +0530

    feat: add signin route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -31,4 +31,25 @@ userRouter.post("/signup", async (c) => {
   });
 });
 
-userRouter.post("/signin", async (c) => {});
+userRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" });
+  }
+
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
+});
