90ba4087586d53c45eabcdcacde1ac5166fc554b
Niladri Ghosh <niladri1406@gmail.com>
2024-02-25 17:53:54 +0530

    feat: add bogRoute and post for root

---
+++ server/src/routes/blog.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { verify } from "hono/jwt";
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+blogRouter.post("/", async (c) => {
+  const userId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const post = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: userId,
+    },
+  });
+  return c.json({
+    id: post.id,
+  });
+});
+
+blogRouter.put("/", async (c) => {});
+
+blogRouter.get("/:id", async (c) => {});
